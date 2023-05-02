62c05c87ed213aa725d0c07da943e46d3c9cbd64
Dave Nash <davidjnash2@gmail.com>
2023-05-02 10:44:13 -0500

    sourced js in html, pushed tile objects to array, wrote function to get tile objects sum

--- client.js
+++ client.js
@@ -10,8 +10,10 @@ console.log('Step 0 complete! client.js loaded!');
 // Write your code here!
 
 // 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
+let highScore = 29;
 
 // 2. Make a new empty array called myScrabbleTiles.
+let myScrabbleTiles = []; //array to hold tile objects
 
 // 3. Put these 6 tile objects into myScrabbleTiles:
 /*
@@ -22,22 +24,40 @@ console.log('Step 0 complete! client.js loaded!');
   { tile: 'D', score: 2 }
   { tile: 'A', score: 1 }
 */
+myScrabbleTiles.push({ tile: 'N', score: 1 });
+myScrabbleTiles.push({ tile: 'K', score: 5 });
+myScrabbleTiles.push({ tile: 'Z', score: 10 });
+myScrabbleTiles.push({ tile: 'X', score: 8 });
+myScrabbleTiles.push({ tile: 'D', score: 2 });
+myScrabbleTiles.push({ tile: 'A', score: 1 });
+
+console.log(myScrabbleTiles);
 
 // 4. Remove the last tile from myScrabbleTiles.
-
+let lostTile = myScrabbleTiles.pop();
+console.log('goodbye', lostTile);
 
 // 5. Add the following new tile to myScrabbleTiles:
 /*
   { tile: 'F', score : 4 }
 */
+myScrabbleTiles.push({ tile: 'F', score: 4 });
 
-// 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
+
+// 6. Complete this function. It needs to be given an array of tile objects. 
+// The function will use a for-loop and return the sum of all the scores in the given array.
 function sumTiles(anArray) {
-
+  let sum = 0;
+  for (let i=0; i<anArray.length; i++){
+    sum = sum + anArray[i].score;
+  }
+  return sum;
 }
+console.log('sumTiles returns:', sumTiles(myScrabbleTiles));
 
 
-// 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
+// 7. Use the function above to get the total score for myScrabbleTiles and assign the 
+// value to a variable called "myScore".
 
 
 // 8. Check whether or not your score is higher than the highScore.
